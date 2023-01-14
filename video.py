135859dfbf35b804059e3a34841265b369e61ff0
davideferrari95 <davideferr@unimore.it>
2023-01-14 12:36:56 +0100

    Increased Video Dimension

--- video.py
+++ video.py
@@ -5,7 +5,7 @@ import utils
 
 
 class VideoRecorder(object):
-    def __init__(self, root_dir, height=256, width=256, camera_id=0, fps=30):
+    def __init__(self, root_dir, height=1024, width=1024, camera_id=0, fps=30):
         self.save_dir = utils.make_dir(root_dir, "video") if root_dir else None
         self.height = height
         self.width = width
