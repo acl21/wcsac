07847ca8281f3cdd9e3e7d9e55e9f81b6e847728
davideferrari95 <davideferr@unimore.it>
2023-01-16 23:20:53 +0100

    Fixed Custom Environment
    max_episode_step=1000

--- utils.py
+++ utils.py
@@ -72,6 +72,7 @@ def make_custom_env(cfg):
         register(
             id="StaticEnv-v0",
             entry_point="safety_gym.envs.mujoco:Engine",
+            max_episode_steps=1000,
             kwargs={"config": config1},
         )
         env = gym.make("StaticEnv-v0", render_mode="rgb_array")
@@ -94,6 +95,7 @@ def make_custom_env(cfg):
         register(
             id="DynamicEnv-v0",
             entry_point="safety_gym.envs.mujoco:Engine",
+            max_episode_steps=1000,
             kwargs={"config": config2},
         )
         env = gym.make("DynamicEnv-v0", render_mode="rgb_array")
